d898e0db0bbffcf85f3e6df0d8868fd7854ac12b
zmengyuan <zmengyuan@126.com>
2018-03-06 16:06:09 +0800

    wechat the first

---
+++ src/main/java/com/zmy/wechatsell/WechatsellApplication.java
@@ -0,0 +1,12 @@
+package com.zmy.wechatsell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatsellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatsellApplication.class, args);
+	}
+}
