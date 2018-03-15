f798db9444d7e0d06c2534ec9eabbe8bd6c5da95
zmengyuan <zmengyuan@126.com>
2018-03-15 13:26:51 +0800

    modify summary.txt

--- src/test/java/com/zmy/wechatsell/WechatsellApplicationTests.java
+++ src/test/java/com/zmy/wechatsell/WechatsellApplicationTests.java
@@ -1,16 +1,28 @@
 package com.zmy.wechatsell;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class WechatsellApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void test1() {
+		BigDecimal bigDecimal = new BigDecimal(BigInteger.ZERO);
+
+		log.info("bigDecimal={}",bigDecimal);
 	}
 
 }
