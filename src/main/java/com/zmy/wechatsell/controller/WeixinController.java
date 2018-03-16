f8deff5d8a219cdf75b9b4190c88b12c1c2edd63
zmengyuan <zmengyuan@126.com>
2018-03-16 14:38:10 +0800

    2018/3/16

--- src/main/java/com/zmy/wechatsell/controller/WeixinController.java
+++ src/main/java/com/zmy/wechatsell/controller/WeixinController.java
@@ -18,9 +18,13 @@ public class WeixinController {
     @GetMapping(value = "/auth")
     public void auth(@RequestParam("code") String code) {
         log.info("进入auth方法");
-        log.info("codeL{}",code);
+        log.info("code={}",code);
 
         String url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxf82672541effbe0e&secret=de59f601c4b564fbf7f90dc7f76d6664&code="+code+"&grant_type=authorization_code";
 
+        RestTemplate restTemplate = new RestTemplate();
+        //getForObject()方法：发送一个get方法，返回的请求体将映射为一个对象
+        String response = restTemplate.getForObject(url, String.class);
+        log.info("response={}", response);
     }
 }
